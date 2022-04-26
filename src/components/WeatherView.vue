474c932dabeff2c9d3eb695ebab620365d137d56
inayaka <ina4@wp.eu>
2022-04-26 18:31:29 +0200

    vue_18_31

---
+++ src/components/WeatherView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="WeatherView">
+    <h1>Weather</h1>
+    {{weather}}
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WeatherView',
+    data(){
+        return{
+            weather: "sun",
+        }
+    }
+
+}
+</script>
+
+<style>
+
+
+</style>
+
